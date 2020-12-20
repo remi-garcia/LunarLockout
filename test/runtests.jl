c2acc612aee9460f516b536fb12e62b8afff9cfb
Rémi <47191601+remi-garcia@users.noreply.github.com>
2020-12-20 01:50:11 +0100

    Add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,12 @@ using Test
 
 @testset "Instances" begin
     for file in readdir((@__DIR__)*"/../instances/")
-        @test !isempty(find_solution(read_problem(file)))
+        @test !isempty(find_solution(read_problem((@__DIR__)*"/../instances/"*file)))
     end
+end
+
+
+@testset "No solution" begin
+    @test isempty(find_solution(read_board("""12\n\n\n\n""")))
+    @test_skip isempty(find_solution(read_board("""1 2 3\n\n\n\n""")))
 end
